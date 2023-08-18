c7800cc22162c2a227c3a380bb44e001fa11e61f
Daniel Lema <103025222+Dande8719@users.noreply.github.com>
2023-08-18 17:54:20 +0200

    Add files via upload

--- st_REE.py
+++ st_REE.py
@@ -13,7 +13,7 @@ from streamlit_folium import st_folium
 
 df_demanda_nacional = pd.read_csv('demanda_nacional.csv')
 df_demanda_comunidades = pd.read_csv('demanda_comunidades.csv')
-df_Emisionas_diarias = pd.read_csv('EmisionesDiarias.csv')
+df_Emisiones_diarias = pd.read_csv('Emisiones.csv')
 df_Gener_dia_ren_no_ren= pd.read_csv('GeneracionDiariaRenNoRen.csv')
 df_intercambio= pd.read_csv('intercambio.csv')
 df_gen_dia_x_tecno= pd.read_csv('GeneracionDiariaXTecnologia.csv')
@@ -24,11 +24,11 @@ def main():
 
     #Definimos un tÍtulo general para la página.
     st.header("MODELOS DE PREDICCIÓN DE SERIES TEMPORALES CON REDES NEURONALES")
-    st.markdown(body = '<div style="text-align: justify; font-size: 20px; color:Gray;">Usamos estos modelos para predecir diferentes variables del sistema electrico español</div>',
-                unsafe_allow_html = True)
-    #st.markdown("#### Usamos estos modelos para predecir diferentes variables del sistema electrico español")
+    # st.markdown(body = '<div style="text-align: justify; font-size: 20px; color:Gray;">Usamos estos modelos para predecir diferentes variables del sistema electrico español</div>',
+    #             unsafe_allow_html = True)
+    st.markdown("#### Usamos estos modelos para predecir diferentes variables del sistema electrico español")
     col1, col2, col3 = st.columns([1, 6, 1])
-    col4, col5, col6 = st.columns([2, 4, 2])
+    col4, col5, col6 = st.columns([3, 2, 3])
     #Introcucimos imagenes principales para la página.
     col2.image("Imgn_REE.JPG", use_column_width = True,)
     col5.image("Imgn_REE_2.JPG", use_column_width = True)
@@ -101,7 +101,8 @@ def main():
                 fill_color="OrRd",
                 data=df_com,
                 columns=["Comunidades", "media"],
-                key_on="feature.properties.name"
+                key_on="feature.properties.name",
+                title="Demanda Eléctrica Nacional"
             ).add_to(spain_map)
             return spain_map
         
@@ -119,9 +120,26 @@ def main():
             st.dataframe(df_Gener_dia_ren_no_ren)
             st.dataframe(df_gen_dia_x_tecno)
         #Texto
-        st.write('Texto modelo generación mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm n\
-                 mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm n\
-                 mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm')
+        st.write('Los datos de generación de energía eléctrica son datos diarios a nivel nacional y divididos por n\
+                 tecnologías de generación, tanto renovables como no renovables. En el siguiente grafico n\
+                  podemos ver e interactuar con los distintos métodos de generación de energía desde 2014.')
+
+        
+        df_gen_dia_x_tecno["Fechas"]= df_gen_dia_x_tecno["Fechas"].apply(lambda x : x.split("-")[0])
+        df_gen_dia_x_tecno.groupby("Fechas") 
+        # df_gen_dia_x_tecno.drop(df_gen_dia_x_tecno.index[0], inplace=True) como elimino 2014 y 2023
+
+        #Imagen relativa a la generación
+        fig3=px.bar(data_frame = df_gen_dia_x_tecno,
+                x= "Fechas",
+                y= df_gen_dia_x_tecno.columns[1:],
+                title="Generación energética por tipo de tecnología utilizada")
+        st.plotly_chart(figure_or_data = fig3, use_container_width = True)
+
+        #fig.write_image("Gen_tec_ano.jpg", format="jpeg")
+
+
+
 
     ##################################### EMISIONES ################################################
     ################################################################################################
@@ -131,14 +149,21 @@ def main():
         st.title('MODELO EMISIONES')
         #DataFrame
         with st.expander('Dataframe Emisiones', False):
-            st.dataframe(df_Emisionas_diarias)
+            st.dataframe(df_Emisiones_diarias)
             
         #Texto
-        st.write('Texto modelo Emisiones mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm n\
-                 mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm n\
-                 mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm')   
+        st.write('Los datos de emisiones de CO2 al igual que la generación son datos diarios obtenidos a nivel n\
+                  nacional y divididos por tecnologías. En este grafico podemos interaccionar con las emisiones n\
+                 de las diferentes tecnologías.') 
 
         
+
+        fig4=px.pie(data_frame = df_Emisiones_diarias,
+             names= "Tecnologías",
+             values= "% Emisiones")
+        
+        st.plotly_chart(figure_or_data = fig4, use_container_width = True)
+
     ############################# CARGAMOS IMÁGENES Y PIE  DE PÁGINA #######################
     ###############################################################################################
     ###############################################################################################    
@@ -146,7 +171,7 @@ def main():
     #tamaño_deseado_im_HaB = (300, 200) 
     st.markdown("***")
     st.image("Imgn_HaB.JPG", width=80)
-    st.text('Autores del trabajo: Aitor, Eva, Adrian y Daniel Lema           Tutor: Daniel Tummler')
+    st.text('Autores del trabajo: Aitor, Eva, Adrian y Daniel Lema          Tutor: Daniel Tummler ')
     
     
     ############################# CERRAMOS ESTRUCTURA DEL MODELO ########################
